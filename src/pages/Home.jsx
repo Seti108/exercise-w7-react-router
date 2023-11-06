b52942d18e292be81cbf0f258b6fc51e9c8aa4df
SETI108 <seti108@gmail.com>
2023-11-06 12:01:27 +0100

    Exercise done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 // TODO: Import necessary modules and data
+import { Link } from "react-router-dom";
+import data from "../data/songs.json";
 function Home() {
   // TODO: Implement logic to display the list of songs
+  const songData = data.songs;
   return (
     <>
       <div>
         <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
-        {/* TODO: Display the list of songs with links */}
+        <ol>
+          {songData.map((song, index) => {
+            return (
+              <Link to={`/song/${song.title}`} key={index}>
+                <li>{song?.title || "Song not found"}</li>
+              </Link>
+            );
+          })}
+        </ol>
       </div>
     </>
   );
--- src/pages/SongInfo.jsx
+++ src/pages/SongInfo.jsx
@@ -1,17 +1,34 @@
 // TODO: Import necessary modules and data
-
+import { useParams } from "react-router-dom";
+import data from "../data/songs.json";
 // TODO: Destrcutrue props to receive information coming in from the parent comp
 function SongInfo() {
   // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
+  const songParams = useParams();
+  const songData = data.songs;
+
+  const songInfo = songData.find((song) => {
+    return song.title === songParams.songTitle;
+  });
 
   return (
     <>
       <div>
-        <h2>Song Information</h2>
-        {/* TODO: Display song details or a "Song not found!" message */}
+        <h2>{songInfo?.title || "No title found"}</h2>
+        <p>Rank: {songInfo?.rank || "Rank not found"}</p>
+        <p>Rank: {songInfo?.artist || "Artist not found"}</p>
+        <p>Album: {songInfo?.album || "Album not found"}</p>
+        <p>Year: {songInfo?.year || "Year not found"}</p>
       </div>
     </>
   );
+
+  songInfo.defaultProps = {
+    title: "No title found",
+    rank: "Rank not found",
+    album: "No album found",
+    year: "Year not found",
+  };
 }
 
 export default SongInfo;
